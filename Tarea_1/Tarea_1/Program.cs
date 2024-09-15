1dcd15deb786aa5f4a04b873710d588ca70402e8
RicardoFLD <63625002+RicardoFLD@users.noreply.github.com>
2024-09-15 12:40:32 -0600

    ---

---
+++ Tarea_1/Tarea_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+
+            int suma = 2 + 5;
+
+            Console.WriteLine("la suma de 2 + 5 es: " + suma);
+
+            Console.ReadKey();
+        }
+    }
+}
